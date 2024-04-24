4fdd91e02f280a7636d96b8791127d1e0bd048d0
Supriya Roy <supriyaroy.982000@gmail.com>
2024-04-24 20:58:16 +0530

    Returning Object as JSON/XML

--- src/main/java/com/supriya/ui/controller/UserController.java
+++ src/main/java/com/supriya/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.supriya.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,38 +13,40 @@ import org.springframework.web.bind.annotation.RestController;
 import com.supriya.ui.model.response.UserRest;
 
 @RestController
-@RequestMapping("/users")// https://localhost:8080/users
+@RequestMapping("/users") // https://localhost:8080/users
 public class UserController {
-	
+
 	@GetMapping
-	public String getUser(@RequestParam(value="page", defaultValue="1") int page,
-			@RequestParam(value="limit", defaultValue="50") int limit,
-			@RequestParam(value="sort",defaultValue="desc", required = false) String sort)
-	{
+	public String getUser(@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "limit", defaultValue = "50") int limit,
+			@RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
 		return "get user was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
 	}
-	
-	@GetMapping(path="/{userId}")
+
+	@GetMapping(path = "/{userId}", produces = {
+			MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE 
+			})
 	public UserRest getUser(@PathVariable("userId") String userId) {
 		UserRest returnValue = new UserRest();
 		returnValue.setEmail("supriyaroy@gmail.com");
 		returnValue.setFirstName("Supriya");
 		returnValue.setLastName("Roy");
-		
+
 		return returnValue;
-		
+
 	}
-	
+
 	@PostMapping
 	public String createUser() {
 		return "create user was called";
 	}
-	
+
 	@PutMapping
 	public String updateUser() {
 		return "update user was called";
 	}
-	
+
 	@DeleteMapping
 	public String deleteUser() {
 		return "delete user was called";
