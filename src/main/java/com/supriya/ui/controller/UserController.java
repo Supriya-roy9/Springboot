9a37cb3d21fdaf88e77dcdc3023980aea08641ec
Supriya Roy <supriyaroy.982000@gmail.com>
2024-04-24 19:09:57 +0530

    Request Prameters

--- src/main/java/com/supriya/ui/controller/UserController.java
+++ src/main/java/com/supriya/ui/controller/UserController.java
@@ -6,14 +6,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")// https://localhost:8080/users
 public class UserController {
 	
+	@GetMapping
+	public String getUser(@RequestParam(value="page") int page,
+			@RequestParam(value="limit") int limit) {
+		return "get user was called with page = " + page + " and limit = " + limit;
+	}
+	
 	@GetMapping(path="/{userId}")
-	public String getUser(@PathVariable String userId) {
+	public String getUser(@PathVariable("userId") String userId) {
 		return "get user was called with userId = " + userId;
 	}
 	
